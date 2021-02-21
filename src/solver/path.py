ea6830c1842b6e64cee0df59fe209e32db917467
OrtwIn <bertil.braun.private@gmail.com>
2021-02-21 16:00:56 +0100

    Added color to Path finding

--- src/solver/path.py
+++ src/solver/path.py
@@ -15,12 +15,18 @@ class Path:
             print(node)
 
     def save(self, base_path: str, save_to_path: str) -> None:
-        import numpy as np
         from PIL import Image
 
-        array = np.asarray(Image.open(base_path)).copy()
-        for node in self.path:
-            array[node.pos.x, node.pos.y] = 128
+        colors = []
+        for i in range(len(self.path)):
+            fract = i / len(self.path)
+            colors.append((int(fract * 240), 0, int((1 - fract) * 240)))
 
-        im = Image.fromarray(array)
-        im.save(save_to_path)
+        img = Image.open(base_path)
+        rgb_img = Image.new("RGBA", img.size)
+        rgb_img.paste(img)
+
+        for i, node in enumerate(self.path):
+            rgb_img.putpixel((node.pos.x, node.pos.y), colors[i])
+
+        rgb_img.save(save_to_path)
